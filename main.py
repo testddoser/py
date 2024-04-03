7aad6790c54018ff7227b1db1ed1e564bc2d5406
testddoser <129606911+testddoser@users.noreply.github.com>
2024-04-03 20:28:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ bot.send_message(1199404728, "!BOT STARTED!")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello! I'm a simple bot. Type something and I will try to answer.")
+    bot.reply_to(message, "Я отсылаю сообщение каждые 20 секнуд")
 
-@bot.message_handler(content_types=['text'])
-def handle_message(message):
-    # Отвечаем на сообщение пользователя
-    bot.reply_to(message, message.text)
+while True:
+    time.sleep(20)
+    bot.send_message(1199404728, "Провшло 20 секунд")
 
 # Запускаем бота
 bot.infinity_polling()
